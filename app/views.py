41ae7460f179e558e1243c59160e39735b503fd9
Tare' Gaskin <tare.dgg@gmail.com>
2017-04-11 13:28:36 -0400

    some tweaks

--- app/views.py
+++ app/views.py
@@ -5,20 +5,22 @@ import praw
 import random
 
 #Reddit Authentication
-reddit = praw.Reddit(client_id = '', client_secret = '', user_agent ='')
+reddit = praw.Reddit(client_id = 'BtMcH7T6b8lFVg', client_secret = 'g09qUNlgWGY48KCWy7iCvK6Bah4', user_agent ='web:yourenotalone_us:v0.0.1 (by /u/yourenotalone_us')
 subreddit = reddit.subreddit('depression')
 dpostdic = {}
 
 #gets title, post pairs
 def get_pairs():
-    for submission in subreddit.hot(limit=25):
+    for submission in subreddit.hot(limit=50):
         if 100 > submission.score:
             dpostdic[str(submission.title)] = submission.selftext
     return dpostdic
 
-get_pairs()
-#choose a random title, post pair from the above dictionary
-d_title, d_post = random.choice(list(dpostdic.items()))
+def clean_pairs():
+    for key, value in dpostdic.items():
+        if 'reddit' in (key or value):
+            del dpostdic[value]
+
 
 #Copypaste from Flask Tutorial Site (Only half certain what this doesx)
 @app.route('/')
@@ -26,5 +28,9 @@ d_title, d_post = random.choice(list(dpostdic.items()))
 
 
 def index():
-    d_user = "Replace_DUser"
-    return render_template('index.html', d_title = d_title, d_post = d_post, d_user = d_user)
+    get_pairs()
+    clean_pairs()
+    #choose a random title, post pair from the above dictionary
+    d_title, d_post = random.choice(list(dpostdic.items()))
+    d_user = "-Anonymous Writer" + " " + str(random.choice(range(0,1000)))
+    return render_template('index.html', d_title = d_title.capitalize(), d_post = d_post, d_user = d_user)
